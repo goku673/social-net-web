12560b87de618293c07d1fa3e5bbb1f56b7e9d49
Freddy Amin Zapata <zapatamorato@gmail.com>
2025-12-08 20:06:35 -0300

    fix: format correct 'to

--- src/app/(pages)/socialNet/[user]/page.js
+++ src/app/(pages)/socialNet/[user]/page.js
@@ -1,16 +1,16 @@
-"use client"
-import React from 'react'
+'use client';
+import React from 'react';
 import { useParams } from 'next/navigation';
 import UserProfile from '@/app/components/userProfile';
 
 const UserProfilePage = () => {
-    const { user} = useParams();
+  const { user } = useParams();
   return (
     <div>
-       holaa {user}
-       <UserProfile/>
+      holaa {user}
+      <UserProfile />
     </div>
-  )
-}
+  );
+};
 
 export default UserProfilePage;
